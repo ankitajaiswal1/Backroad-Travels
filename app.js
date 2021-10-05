f1a85b38d89beda2a43fd62c42260c0f6dd39656
ankita <ankuankita920@gmail.com>
2021-10-05 18:59:40 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// 
+const navBtn = document.getElementById("nav-toggle");
+const  links = document.getElementById("nav-links");
+//add event listner
+navBtn.addEventListener("click", () => {
+    links.classList.toggle("show-links");
+});
+//select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+    link.addEventListener("click", e => {
+        //prevent default
+        e.preventDefault();
+        links.classList.remove("show-links");
+
+        const id = e.target.getAttribute("here").slice(1);
+        const element = document.getElementById(id);
+        //
+        let position = element.offsetTop - 62;
+        window.scrollTo({
+            left: 0,
+            //top: element.offsetTop,
+            top: position,
+            behaviour: "smooth"
+        });
+    });
+});
